a4b7b67d1583fb70d8913b7e5765ba94a9a7cb10
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-04-08 00:13:59 +0400

    falsy vallues

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -47,3 +47,11 @@ n = n - 1;
 
 console.log(2 + 3 - "4" + 4 - 2 + "4");
 console.log("23" - 3 + 4 - "4");
+
+// falsy values: 0, '', undefined, null, Nan
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("jonS"));
+console.log(Boolean({}));
+console.log(Boolean(""));
