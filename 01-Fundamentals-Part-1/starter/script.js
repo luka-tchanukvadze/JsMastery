84274619ca1ecaa8536b68d462b79f5fa67c089f
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-04-08 00:18:02 +0400

    truthy and falsy values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -55,3 +55,17 @@ console.log(Boolean(undefined));
 console.log(Boolean("jonS"));
 console.log(Boolean({}));
 console.log(Boolean(""));
+
+const money = 0;
+if (money) {
+  console.log("Don't spend it all");
+} else {
+  console.log("You should get a job");
+}
+
+let height;
+if (height) {
+  console.log("yay, it is defined");
+} else {
+  console.log("it is undefined");
+}
