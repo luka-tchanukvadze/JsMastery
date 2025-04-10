40daeccc1612ce5cbe26a8d925daf9afcb80b043
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-04-10 22:54:55 +0400

    functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,11 +1,31 @@
 "use strict";
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-// if (passTest) hasDriverLicense = true;
+// // if (passTest) hasDriverLicense = true;
 
-if (hasDriversLicense) console.log("i can drive");
+// if (hasDriversLicense) console.log("i can drive");
 
-// const interface = "Audio";
-// const private = 43
+// // const interface = "Audio";
+// // const private = 43
+
+//////////////////////////////// functions
+
+function logger() {
+  console.log("Something");
+}
+
+// calling / running / involing function
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  // parameters
+  console.log(apples, oranges);
+  const juice = `juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 6); // arguments
+console.log(appleJuice);
