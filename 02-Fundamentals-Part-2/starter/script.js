9fae035d44640e31a28551c60a82769294739415
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-04-22 22:56:23 +0400

    challenge 3

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -218,7 +218,7 @@ const mark = {
   fullName: "Mark Miller",
   mass: 78,
   height: 1.69,
-  calcMBI: function () {
+  calcBMI: function () {
     this.BMI = this.mass / this.height ** 2;
     return this.BMI;
   },
@@ -228,7 +228,7 @@ const john = {
   fullName: "John Smith",
   mass: 92,
   height: 1.95,
-  calcMBI: function () {
+  calcBMI: function () {
     this.BMI = this.mass / this.height ** 2;
     return this.BMI;
   },
