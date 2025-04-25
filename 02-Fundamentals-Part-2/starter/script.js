5f792c87c82e464437246027d71519a8c4b08d54
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-04-26 00:21:47 +0400

    clean up

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -336,29 +336,29 @@ const chanu = ["chanu", "luka", 4324, "legend", ["fucking", "legend"]];
 //   4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements)
 //   4.3. Call the function with the 'totals' array
 
-const calcTip = (bill) => {
-  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-};
+// const calcTip = (bill) => {
+//   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// };
 
-const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
-const tips = [];
-const totals = [];
+// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// const tips = [];
+// const totals = [];
 
-for (let i = 0; i < bills.length; i++) {
-  const tip = calcTip(bills[i]);
-  tips.push(tip);
-  totals.push(tip + bills[i]);
-}
+// for (let i = 0; i < bills.length; i++) {
+//   const tip = calcTip(bills[i]);
+//   tips.push(tip);
+//   totals.push(tip + bills[i]);
+// }
 
-console.log(bills, tips, totals);
+// console.log(bills, tips, totals);
 
-const caclAverage = function (arr) {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
-  }
-  console.log(sum);
-  return sum / arr.length;
-};
+// const caclAverage = function (arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//   }
+//   console.log(sum);
+//   return sum / arr.length;
+// };
 
-console.log(caclAverage([2, 3, 5]));
+// console.log(caclAverage([2, 3, 5]));
