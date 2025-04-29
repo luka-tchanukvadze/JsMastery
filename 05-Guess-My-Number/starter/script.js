9e3cf627e6d7ef92db954ec6713f61617e55beeb
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-04-29 20:28:19 +0400

    click events

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,9 +1,18 @@
 'use strict';
 
-document.querySelector('.message').textContent = 'Correct Number';
+// document.querySelector('.message').textContent = 'Correct Number';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 20;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 20;
 
-document.querySelector('.guess').value = 33;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 33;
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  }
+});
