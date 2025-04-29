519d8966f3e2e3d8036e5dd63312043f583c5b34
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-04-29 20:21:30 +0400

    Selecting and Manipulating Elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+document.querySelector('.message').textContent = 'Correct Number';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+document.querySelector('.guess').value = 33;
+console.log(document.querySelector('.guess').value);
