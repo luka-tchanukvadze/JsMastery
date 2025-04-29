3937f97109562c10adeaafeda750d9d76b8a31b5
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-04-29 20:37:49 +0400

    implementing the game logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,12 +1,9 @@
 'use strict';
 
-// document.querySelector('.message').textContent = 'Correct Number';
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 20;
-
-// document.querySelector('.guess').value = 33;
-// console.log(document.querySelector('.guess').value);
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -14,5 +11,15 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too high!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too low!';
+    score--;
+    document.querySelector('.score').textContent = score;
   }
 });
