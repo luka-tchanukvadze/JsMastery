7e5ef281e2c5f174d89e860e55f187b8267482a0
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-05 22:41:20 +0400

    scoping

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -2,7 +2,22 @@
 
 function calcAge(birthYear) {
   const age = 2047 - birthYear;
-  console.log(firstNmae);
+
+  function printAge() {
+    const output = `${firstNmae} you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1982 && birthYear <= 1996) {
+      var millenial = true;
+      const str = `oh and you are a millenial, ${firstNmae}`;
+      console.log(str);
+    }
+
+    // console.log(str)
+    console.log(millenial);
+  }
+  printAge();
+
   return age;
 }
 
