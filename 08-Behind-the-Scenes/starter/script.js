03045fe8acb87f50148e76c006a8b1bb179c6386
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-05 22:45:35 +0400

    scope chain

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -9,12 +9,21 @@ function calcAge(birthYear) {
 
     if (birthYear >= 1982 && birthYear <= 1996) {
       var millenial = true;
+      const fristName = 'Luka';
+
       const str = `oh and you are a millenial, ${firstNmae}`;
       console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+
+      output = 'NEW OUTPUT';
     }
 
     // console.log(str)
     // console.log(millenial);
+    // console.log(add(2, 4));
   }
   printAge();
 
