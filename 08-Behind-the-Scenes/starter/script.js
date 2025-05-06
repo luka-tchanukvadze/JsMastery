44cae9260938331877fadb1a6b911aca61174766
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-06 18:06:01 +0400

    Hoisting and TDZ practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,34 +1,72 @@
-'use strict';
+// 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2047 - birthYear;
+// function calcAge(birthYear) {
+//   const age = 2047 - birthYear;
 
-  function printAge() {
-    const output = `${firstNmae} you are ${age}, born in ${birthYear}`;
-    console.log(output);
+//   function printAge() {
+//     const output = `${firstNmae} you are ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-    if (birthYear >= 1982 && birthYear <= 1996) {
-      var millenial = true;
-      const fristName = 'Luka';
+//     if (birthYear >= 1982 && birthYear <= 1996) {
+//       var millenial = true;
+//       const fristName = 'Luka';
 
-      const str = `oh and you are a millenial, ${firstNmae}`;
-      console.log(str);
+//       const str = `oh and you are a millenial, ${firstNmae}`;
+//       console.log(str);
 
-      function add(a, b) {
-        return a + b;
-      }
+//       function add(a, b) {
+//         return a + b;
+//       }
 
-      output = 'NEW OUTPUT';
-    }
+//       output = 'NEW OUTPUT';
+//     }
 
-    // console.log(str)
-    // console.log(millenial);
-    // console.log(add(2, 4));
-  }
-  printAge();
+//     // console.log(str)
+//     // console.log(millenial);
+//     // console.log(add(2, 4));
+//   }
+//   printAge();
 
-  return age;
+//   return age;
+// }
+
+// const firstNmae = 'Luka';
+// calcAge(2003);
+
+///////////////////////////////////// hoisting
+
+// Variables
+// console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'luka';
+let job = 'fd';
+const year = 1991;
+
+////
+
+// Functions
+// console.log(addDecl(53, 2));
+// console.log(addExpr(3, 5));
+// console.log(addArrow(3, 4));
+
+function addDecl(a, b) {
+  return a + b;
 }
 
-const firstNmae = 'Luka';
-calcAge(2003);
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
+
+//////////////////////// example
+
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('all products deleted');
+}
