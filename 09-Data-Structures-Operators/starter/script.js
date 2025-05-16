c1f969c6f83e9fd80e36a8915d738fbb8567afc8
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-16 21:34:41 +0400

    clean up

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -534,31 +534,31 @@ GOOD LUCK 😀
 // rest.set(arr, 'Test');
 // console.log(rest.get(arr));
 
-const question = new Map([
-  ['question', 'what is the best programming language in the world'],
-  [1, 'C'],
-  [2, 'Java'],
-  [3, 'JavaScript'],
-  ['correct', 3],
-  [true, 'Correct'],
-  [false, 'Try again!'],
-]);
+// const question = new Map([
+//   ['question', 'what is the best programming language in the world'],
+//   [1, 'C'],
+//   [2, 'Java'],
+//   [3, 'JavaScript'],
+//   ['correct', 3],
+//   [true, 'Correct'],
+//   [false, 'Try again!'],
+// ]);
 
-// Conver object to map
-const hoursMap = new Map(Object.entries(openingHours));
-console.log(hoursMap);
+// // Conver object to map
+// const hoursMap = new Map(Object.entries(openingHours));
+// console.log(hoursMap);
 
-console.log(question.question);
-for (const [key, value] of question) {
-  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
-}
+// console.log(question.question);
+// for (const [key, value] of question) {
+//   if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+// }
 
-const answer = prompt('Your answer');
-console.log(answer);
+// const answer = prompt('Your answer');
+// console.log(answer);
 
-console.log(question.get(question.get('correct') === answer));
+// console.log(question.get(question.get('correct') === answer));
 
-console.log([...question]);
-console.log(...question.entries());
-console.log(...question.keys());
-console.log(...question.values());
+// console.log([...question]);
+// console.log(...question.entries());
+// console.log(...question.keys());
+// console.log(...question.values());
