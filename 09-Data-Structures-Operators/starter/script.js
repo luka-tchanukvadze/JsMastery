8fc326a46fb743e0ecd61ac51aae5439687ed9ac
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-18 19:41:29 +0400

    string methods practice

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -740,17 +740,34 @@ Afterwards, test with your own test data!
 GOOD LUCK 😀
 */
 
-document.body.append(document.createElement('textarea'));
-document.body.append(document.createElement('button'));
+// document.body.append(document.createElement('textarea'));
+// document.body.append(document.createElement('button'));
 
-document.querySelector('button').addEventListener('click', function () {
-  const text = document.querySelector('textarea').value;
-  const rows = text.split('\n');
+// document.querySelector('button').addEventListener('click', function () {
+//   const text = document.querySelector('textarea').value;
+//   const rows = text.split('\n');
 
-  for (const [i, row] of rows.entries()) {
-    const [first, second] = row.toLowerCase().trim().split('_');
-    const output = `${first}${second.replace(second[0].toUpperCase())}`;
+//   for (const [i, row] of rows.entries()) {
+//     const [first, second] = row.toLowerCase().trim().split('_');
+//     const output = `${first}${second.replace(second[0].toUpperCase())}`;
 
-    console.log(`${output.padEnd(20)}${'W'.repeat(i + 1)}`);
-  }
-});
+//     console.log(`${output.padEnd(20)}${'W'.repeat(i + 1)}`);
+//   }
+// });
+
+const flight =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+console.log(flight.split('+'));
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const fl of flight.split('+')) {
+  const [type, from, to, time] = fl.split(';');
+  const output = `${type.startsWith('_Delayed') ? 'O' : ''} ${type.replaceAll(
+    '_',
+    ' '
+  )} ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`.padStart(36);
+
+  console.log(output);
+}
