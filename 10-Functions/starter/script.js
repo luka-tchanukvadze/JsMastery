816691fa35e5a72c0b810d13790544a594a8499b
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-19 18:07:55 +0400

    How Passing Arguments Works Value vs. Reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,26 +1,56 @@
 'use strict';
 
-const bookings = [];
+// const bookings = [];
 
-const createBooking = function (
-  flightNum,
-  numPassengers = 1,
-  price = 199 * numPassengers
-) {
-  // ES5
-  // numPassengers = numPassengers || 1;
-  // price = price || 199;
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+//   // ES5
+//   // numPassengers = numPassengers || 1;
+//   // price = price || 199;
 
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
 
-  console.log(booking);
-  bookings.push(booking);
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking('LH123');
+// createBooking('LH123', 3, 800);
+// createBooking('LH123', 3);
+// createBooking('LH123', undefined, 800);
+
+const flight = 'LH123';
+
+const luka = {
+  name: 'Luka Chanu',
+  passport: 32324324324,
 };
 
-createBooking('LH123');
-createBooking('LH123', 3, 800);
-createBooking('LH123', undefined, 800);
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'MR. ' + passenger.name;
+
+  if (passenger.passport === 4324324324) {
+    alert('checked in');
+  } else {
+    alert('wrong passport');
+  }
+};
+
+checkIn(flight, luka);
+console.log(flight);
+console.log(luka);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000);
+};
+
+newPassport(luka);
+checkIn(flight, luka);
