2f1018129b785a91ac05487dd007c2f00f8fe0e0
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-23 18:21:46 +0400

    chlallenge 2

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -315,16 +315,37 @@ GOOD LUCK 😀
 // console.dir(f);
 
 // example 2
-const boardPassengers = function (n, wait) {
-  // const perGroup = n / 3;
+// const boardPassengers = function (n, wait) {
+//   // const perGroup = n / 3;
 
-  setTimeout(function () {
-    console.log(`We are now boarding all ${n} passengers`);
-    console.log(`there are 3 groups, each with ${perGroup} passengers`);
-  }, wait * 1000);
+//   setTimeout(function () {
+//     console.log(`We are now boarding all ${n} passengers`);
+//     console.log(`there are 3 groups, each with ${perGroup} passengers`);
+//   }, wait * 1000);
 
-  console.log(`we will satrt boarding in ${wait} seconds`);
-};
+//   console.log(`we will satrt boarding in ${wait} seconds`);
+// };
 
-const perGroup = 1000;
-boardPassengers(180, 3);
+// const perGroup = 1000;
+// boardPassengers(180, 3);
+
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+(() => {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
