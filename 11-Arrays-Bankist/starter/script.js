6dd45dea18fa4cf70f0258897c1dd9f4a13809e1
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-26 21:03:01 +0400

    the new 'at' method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -89,30 +89,40 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
-let arr = ['a', 'b', 'c', 'c', 'e'];
+// let arr = ['a', 'b', 'c', 'c', 'e'];
 
-// slice method
-// console.log(arr.slice(2));
-// console.log(arr.slice(2, 4));
-// console.log(arr.slice(-2));
-// console.log(arr.slice(-1));
-// console.log(arr.slice(1, -2));
+// // slice method
+// // console.log(arr.slice(2));
+// // console.log(arr.slice(2, 4));
+// // console.log(arr.slice(-2));
+// // console.log(arr.slice(-1));
+// // console.log(arr.slice(1, -2));
 
-// splice
-// mutates original
-// console.log(arr.splice(2))
-// console.log(arr(-1));
-// console.log(arr);
+// // splice
+// // mutates original
+// // console.log(arr.splice(2))
+// // console.log(arr(-1));
+// // console.log(arr);
 
-// reverse
-// mutates original
-const arr2 = ['j', 'i', 'h', 'g', 'f'];
-console.log(arr2.reverse());
-console.log(arr2);
+// // reverse
+// // mutates original
+// const arr2 = ['j', 'i', 'h', 'g', 'f'];
+// console.log(arr2.reverse());
+// console.log(arr2);
 
-// concat
-const letters = arr.concat(arr2);
-console.log(letters);
+// // concat
+// const letters = arr.concat(arr2);
+// console.log(letters);
 
-// join
-console.log(letters.join('-'));
+// // join
+// console.log(letters.join('-'));
+
+const arr = [23, 11, 13];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+console.log('luka'.at(1));
