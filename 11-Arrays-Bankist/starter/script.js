7d76dad125d5087cb533c296bd61b12fb6bb8999
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-26 20:59:09 +0400

    simple array methodes

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -75,16 +75,44 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+
+/////////////////////////////////////////////////
+
 let arr = ['a', 'b', 'c', 'c', 'e'];
 
 // slice method
-console.log(arr.slice(2));
-console.log(arr.slice(2, 4));
-console.log(arr.slice(-2));
-console.log(arr.slice(-1));
-console.log(arr.slice(1, -2));
+// console.log(arr.slice(2));
+// console.log(arr.slice(2, 4));
+// console.log(arr.slice(-2));
+// console.log(arr.slice(-1));
+// console.log(arr.slice(1, -2));
 
 // splice
+// mutates original
 // console.log(arr.splice(2))
 // console.log(arr(-1));
 // console.log(arr);
+
+// reverse
+// mutates original
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// concat
+const letters = arr.concat(arr2);
+console.log(letters);
+
+// join
+console.log(letters.join('-'));
