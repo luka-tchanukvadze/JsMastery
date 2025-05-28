91c472a99ed689b1476b259dcc62f72d00b13163
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-28 18:34:59 +0400

    clean up

---
+++ 11-Arrays-Bankist/starter/practice.js
@@ -0,0 +1,95 @@
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
+
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+
+/////////////////////////////////////////////////
+
+// let arr = ['a', 'b', 'c', 'c', 'e'];
+
+// // slice method
+// // console.log(arr.slice(2));
+// // console.log(arr.slice(2, 4));
+// // console.log(arr.slice(-2));
+// // console.log(arr.slice(-1));
+// // console.log(arr.slice(1, -2));
+
+// // splice
+// // mutates original
+// // console.log(arr.splice(2))
+// // console.log(arr(-1));
+// // console.log(arr);
+
+// // reverse
+// // mutates original
+// const arr2 = ['j', 'i', 'h', 'g', 'f'];
+// console.log(arr2.reverse());
+// console.log(arr2);
+
+// // concat
+// const letters = arr.concat(arr2);
+// console.log(letters);
+
+// // join
+// console.log(letters.join('-'));
+
+// const arr = [23, 11, 13];
+// console.log(arr[0]);
+// console.log(arr.at(0));
+
+// console.log(arr[arr.length - 1]);
+// console.log(arr.slice(-1)[0]);
+// console.log(arr.at(-1));
+
+// console.log('luka'.at(1));
+
+// for (const movement of movements) {
+// for (const [i, movement] of movements.entries()) {
+//   if (movement > 0) {
+//     console.log(i + ' you deposited');
+//   } else {
+//     console.log('you withdrew');
+//   }
+// }
+
+// console.log('//////////foreach//////////');
+
+// movements.forEach(function (movement, index, array) {
+//   if (movement > 0) {
+//     console.log(index + array + ' you deposited');
+//   } else {
+//     console.log('you withdrew');
+//   }
+// });
+
+// foreach and map
+// currencies.forEach(function (value, key, map) {
+//   console.log(`${key}: ${value}`);
+// });
+
+// const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+
+// currenciesUnique.forEach(function (value, key, map) {
+//   console.log(`${key}: ${value}`);
+// });
