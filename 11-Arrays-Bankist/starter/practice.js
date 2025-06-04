dd137a1e77bf05bd4312717287f7bc216508cb6b
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-06-04 18:52:46 +0400

    array grouping

--- 11-Arrays-Bankist/starter/practice.js
+++ 11-Arrays-Bankist/starter/practice.js
@@ -446,12 +446,68 @@ TEST DATA:
 // console.log(overalBalance);
 
 // strings
-const owners = ['jonas', 'zach', 'adam'];
-console.log(owners.sort());
+// const owners = ['jonas', 'zach', 'adam'];
+// console.log(owners.sort());
 
-// numbers
-console.log(movements);
-console.log(movements.sort());
+// // numbers
+// console.log(movements);
+// console.log(movements.sort());
 
-movements.sort((a, b) => b - a);
-console.log(movements);
+// movements.sort((a, b) => b - a);
+// console.log(movements);
+
+const oaccount1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+  type: 'premium',
+};
+
+const oaccount2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+  type: 'standard',
+};
+
+const oaccount3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+  type: 'premium',
+};
+
+const oaccount4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+  type: 'basic',
+};
+
+const oaccounts = [oaccount1, oaccount2, oaccount3, oaccount4];
+
+// console.log(movements);
+
+const groupedMovements = Object.groupBy(movements, movement =>
+  movement > 0 ? 'deposits' : 'withdrawals'
+);
+// console.log(groupedMovements);
+
+const groupedByActivity = Object.groupBy(oaccounts, account => {
+  const movementCount = account.movements.length;
+
+  if (movementCount >= 8) return 'very active';
+  if (movementCount >= 4) return 'active';
+  if (movementCount >= 1) return 'moderate';
+  return 'inactive';
+});
+
+// console.log(groupedByActivity);
+
+// const groupedAccounts = Object.groupBy(oaccounts, account => account.type);
+const groupedAccounts = Object.groupBy(oaccounts, ({ type }) => type);
+console.log(groupedAccounts);
