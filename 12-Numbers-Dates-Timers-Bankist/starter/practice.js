bc81ce6e711df7900f7aef34bada495fd2c0166f
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-06-06 22:40:05 +0400

    random numbers

--- 12-Numbers-Dates-Timers-Bankist/starter/practice.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/practice.js
@@ -1,25 +1,43 @@
-// converting
-console.log(+'23');
+// // converting
+// console.log(+'23');
 
-// parsing
-console.log(Number.parseInt('30px'));
-console.log(Number.parseInt('e30'));
-console.log(Number.parseInt('30px', 10));
+// // parsing
+// console.log(Number.parseInt('30px'));
+// console.log(Number.parseInt('e30'));
+// console.log(Number.parseInt('30px', 10));
 
-console.log(Number.parseFloat('2.5rem'));
-console.log(Number.parseInt('2.5rem'));
+// console.log(Number.parseFloat('2.5rem'));
+// console.log(Number.parseInt('2.5rem'));
 
-// check if value is nan
-console.log(Number.isNaN(20));
-console.log(Number.isNaN('20'));
-console.log(Number.isNaN(+'20'));
-console.log(Number.isNaN(23 / 0));
+// // check if value is nan
+// console.log(Number.isNaN(20));
+// console.log(Number.isNaN('20'));
+// console.log(Number.isNaN(+'20'));
+// console.log(Number.isNaN(23 / 0));
 
-// checking if nvalue is number
-console.log(Number, isFinite(20));
-console.log(Number, isFinite('20'));
-console.log(Number, isFinite(23 / 0));
+// // checking if nvalue is number
+// console.log(Number, isFinite(20));
+// console.log(Number, isFinite('20'));
+// console.log(Number, isFinite(23 / 0));
 
-console.log(Number.isInteger(23));
-console.log(Number.isInteger(23.0));
-console.log(Number.isInteger(23.1));
+// console.log(Number.isInteger(23));
+// console.log(Number.isInteger(23.0));
+// console.log(Number.isInteger(23.1));
+
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3));
+
+console.log(Math.max(5, 234, 23, 52));
+console.log(Math.max(5, 234, 23, 52, '23px'));
+console.log(Math.min(5, 234, 23, 52));
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+console.log(randomInt(10, 20));
+randomInt(0, 3);
