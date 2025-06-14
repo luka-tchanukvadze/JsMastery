39c15183502fb2be3319fcc2f3c3116ca5ade7f8
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-06-14 15:50:48 +0400

    Selecting, Creating, and Deleting Elements

---
+++ 13-Advanced-DOM-Bankist/starter/practice.js
@@ -0,0 +1,13 @@
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+//html collection
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+
+document.getElementsByClassName('btn');
