3cfe7da75913129c84661c3252e11a9aae00f66f
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-06-14 15:59:52 +0400

    Selecting, Creating, and Deleting Elements

--- 13-Advanced-DOM-Bankist/starter/practice.js
+++ 13-Advanced-DOM-Bankist/starter/practice.js
@@ -1,8 +1,9 @@
+// Selection elements
 console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
 
-document.querySelector('.header');
+const header = document.querySelector('.header');
 const allSections = document.querySelectorAll('.section');
 console.log(allSections);
 
@@ -11,3 +12,24 @@ document.getElementById('section--1');
 const allButtons = document.getElementsByTagName('button');
 
 document.getElementsByClassName('btn');
+
+// creating and inserting elements
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookes for improved functionality and analytics'
+message.innerHTML =
+  'We use cookes for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// header.prepend(message);
+// header.appened(message.cloneNode(true));
+header.append(message);
+
+// header.before(message);
+// header.after(message);
+
+// Delete elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
