91e7070a55584e2e0a6026520c0556087b95c10a
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-06-25 14:43:48 +0400

    Setters and Getters

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -104,29 +104,84 @@ const PersonCl = class{}
 */
 
 // class declaration
+// class PersonCl {
+//   constructor(firstName, birthYear) {
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
+//   }
+
+//   // Methods will be added to .prototype property
+//   calcAge() {
+//     console.log(2037 - this.birthYear);
+//   }
+
+//   greet() {
+//     console.log(`Hey ${this.firstName}`);
+//   }
+// }
+
+// const jessica = new PersonCl('Jessica', 1996);
+// console.log(jessica);
+// // PersonCl.prototype.greet = function () {
+// //    console.log(`Hey ${this.firstName}`)
+// // };
+// jessica.greet();
+
+// // 1. Classes are NOT hoisted
+// // 2. Class are first-class citizes
+// // 3. Classes are executed in strict mode
+
+///////////////////////////// getters and setters
+const account = {
+  owner: 'luka',
+  movements: [123, 213, 4324],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+account.latest = 50;
+
 class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
     this.birthYear = birthYear;
   }
 
-  // Methods will be added to .prototype property
   calcAge() {
     console.log(2037 - this.birthYear);
   }
 
   greet() {
-    console.log(`Hey ${this.firstName}`);
+    console.log(`Hey ${this.fullName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // Set a property that already exists
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) {
+      this._fullName = name;
+    } else {
+      alert(`${name} is not a full name!`);
+    }
+  }
+
+  get fullName() {
+    return this._fullName;
   }
 }
 
 const jessica = new PersonCl('Jessica', 1996);
-console.log(jessica);
-// PersonCl.prototype.greet = function () {
-//    console.log(`Hey ${this.firstName}`)
-// };
-jessica.greet();
-
-// 1. Classes are NOT hoisted
-// 2. Class are first-class citizes
-// 3. Classes are executed in strict mode
+// console.log(jessica);
+// jessica.greet();
+console.log(jessica.age);
