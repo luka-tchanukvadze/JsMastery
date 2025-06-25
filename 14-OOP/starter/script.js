03e63b9c07a46adb2847be7aaacf0a635e9fe290
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-06-25 14:31:19 +0400

    ES6 Classes

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -97,3 +97,36 @@ GOOD LUCK 😀
 // bmw.accelerate();
 // bmw.brake();
 // bmw.accelerate();
+
+/*
+class expression
+const PersonCl = class{}
+*/
+
+// class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+// PersonCl.prototype.greet = function () {
+//    console.log(`Hey ${this.firstName}`)
+// };
+jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Class are first-class citizes
+// 3. Classes are executed in strict mode
