ac28f465496d3cde42fc0ae8e00837a12780fd6a
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-07-11 14:17:01 +0400

    Fixing Some Bad Code

--- 17-Modern-JS-Modules-Tooling/starter/clean.js
+++ 17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -1,4 +1,8 @@
-const budget = [
+'use strict';
+
+const { entries } = require('lodash');
+
+const budget = Object.freeze([
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -7,62 +11,84 @@ const budget = [
   { value: -20, description: 'Candy 🍭', user: 'matilda' },
   { value: -125, description: 'Toys 🚂', user: 'matilda' },
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
-];
+]);
 
-const spendingLimits = {
+const spendingLimits = Object.freeze({
   jonas: 1500,
   matilda: 100,
+});
+// when freezing we cann't add new things, but we can update
+// spendingLimits.jay = 200;
+// console.log(spendingLimits);
+
+const getLimit = (limits, user) => limits?.[user] ?? 0;
+
+const addExpense = function (
+  state,
+  limits,
+  value,
+  description,
+  user = 'jonas'
+) {
+  const cleanUser = user.toLowerCase();
+
+  return value <= getLimit(limits, cleanUser)
+    ? [...state, { value: -value, description, user: cleanUser }]
+    : state;
+
+  // budget.push({ value: -value, description, user: cleanUser });
 };
 
-const getLimit = user => spendingLimits?.[user] ?? 0;
+const newBudget1 = addExpense(budget, spendingLimits, 10, 'Pizza 🍕');
+const newBudget2 = addExpense(newBudget1, spendingLimits, 10, 'Pizza 🍕');
+const newBudget3 = addExpense(newBudget2, spendingLimits, 10000, 'Pizza 🍕');
+// console.log(newBudget1);
+// console.log(newBudget2);
+// console.log(newBudget3);
+// console.log(budget);
 
-const addExpense = function (value, description, user = 'jonas') {
-  user = user.toLowerCase();
+// we had inpure function before (array was manipulated by function)
+// addExpense(budget, spendingLimits, 100, 'Going to movies 🍿', 'Matilda');
+// addExpense(budget, spendingLimits, 200, 'Stuff', 'Jay');
 
-  // let lim;
-  // if (spendingLimits[user]) {
-  //   lim = spendingLimits[user];
-  // } else {
-  //   lim = 0;
+// const checkExpenses = function (state, limits) {
+//   return state.map(entry => {
+//     return entry.value < -getLimit(limits, entry.user)
+//       ? { ...entry, flag: 'limit' }
+//       : entry;
+//   });
+//   // for (const entry of budget) {
+//   //   if (entry.value < -getLimit(limits,entry.user)) {
+//   //     entry.flag = 'limit';
+//   //   }
+//   // }
+// };
+
+const checkExpenses = (state, limits) =>
+  state.map(entry =>
+    entry.value < -getLimit(limits, entry.user)
+      ? { ...entry, flag: 'limit' }
+      : entry
+  );
+
+const finalBudget = checkExpenses(newBudget3, spendingLimits);
+console.log(finalBudget);
+
+// Impure
+const logBigExpenses = function (state, bigLimit) {
+  const bigExpenses = state
+    .filter(entry => entry.value <= -limit)
+    .map(entry => entry.description.spendingLimits(-2))
+    .join(' / ');
+
+  // .reduce((atr, cur) => `${str} / ${cur.description.slice(-2)}`, '');
+  console.log(bigExpenses);
+
+  // let output = '';
+  // for (const el of budget) {
+  //   output += el.value <= -limit ? `${el.description.slice(-2)} /` : '';
   // }
-
-  // const limit = spendingLimits[user] ? spendingLimits[user] : 0;
-
-  if (value <= getLimit(user)) {
-    budget.push({ value: -value, description, user });
-  }
+  // output = output.slice(0, -2); // Remove last '/ '
+  // console.log(output);
 };
-addExpense(10, 'Pizza 🍕');
-addExpense(100, 'Going to movies 🍿', 'Matilda');
-addExpense(200, 'Stuff', 'Jay');
-console.log(budget);
-
-const checkExpenses = function () {
-  for (const entry of budget) {
-    // let lim;
-    // if (spendingLimits[entry.user]) {
-    //   lim = spendingLimits[entry.user];
-    // } else {
-    //   lim = 0;
-    // }
-
-    if (entry.value < -getLimit(entry.user)) {
-      entry.flag = 'limit';
-    }
-  }
-};
-checkExpenses();
-
-console.log(budget);
-
-const logBigExpenses = function (limit) {
-  let output = '';
-  for (const el of budget) {
-    output += el.value <= -limit ? `${el.description.slice(-2)} /` : '';
-  }
-  output = output.slice(0, -2); // Remove last '/ '
-  console.log(output);
-};
-logBigExpenses(1000);
-
-// theory lecture
+logBigExpenses(finalBudget, 500);
