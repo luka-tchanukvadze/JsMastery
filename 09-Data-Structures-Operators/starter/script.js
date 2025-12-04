b9681b36e0efe6f99e70079b9e5abbbe4a940550
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-12-04 23:00:16 +0400

    revising

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -755,19 +755,19 @@ GOOD LUCK 😀
 //   }
 // });
 
-const flight =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// const flight =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-console.log(flight.split('+'));
+// console.log(flight.split('+'));
 
-const getCode = str => str.slice(0, 3).toUpperCase();
+// const getCode = str => str.slice(0, 3).toUpperCase();
 
-for (const fl of flight.split('+')) {
-  const [type, from, to, time] = fl.split(';');
-  const output = `${type.startsWith('_Delayed') ? 'O' : ''} ${type.replaceAll(
-    '_',
-    ' '
-  )} ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`.padStart(36);
+// for (const fl of flight.split('+')) {
+//   const [type, from, to, time] = fl.split(';');
+//   const output = `${type.startsWith('_Delayed') ? 'O' : ''} ${type.replaceAll(
+//     '_',
+//     ' '
+//   )} ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`.padStart(36);
 
-  console.log(output);
-}
+//   console.log(output);
+// }
